7a0a76a8b8485bf236ffd6fea0a703f78c76115e
Raisa <raisaerlich@gmail.com>
2022-09-10 16:44:08 -0300

    encriptador js basic

--- app/app.js
+++ app/app.js
@@ -1,13 +1,49 @@
-let textareaInput = document.getElementById("textareaInput");
+textareaInput = document.getElementById("textareaInput");
 botonEncriptar = document.getElementById("botonEncriptar");
 botonDesencriptar = document.getElementById("botonDesencriptar");
+tituloMensaje = document.getElementById("tituloMensaje");
+textoMensaje = document.getElementById("textoMensaje");
 
 let mensaje = textareaInput.value;
+let mensajeEncriptado = [];
 
-botonEncriptar.addEventListener("click", obtenerTexto);
+botonEncriptar.addEventListener("click", encriptar);
 
 botonDesencriptar.addEventListener("click", obtenerTexto);
 
-function obtenerTexto() {
-  console.log(textareaInput.value);
+function encriptar() {
+  mensaje = textareaInput.value;
+  console.log(mensaje);
+  encriptarTexto(mensaje);
+}
+
+function encriptarTexto(mensaje) {
+  for (let i = 0; i < mensaje.length; i++) {
+    let letra = mensaje.charAt(i);
+    encriptarLetra(letra);
+  }
+  tituloMensaje.remove();
+  textoMensaje.innerText = mensajeEncriptado.join("");
+  mensajeEncriptado = [];
+}
+
+function encriptarLetra(e) {
+  switch (e) {
+    case "e":
+      e = "enter";
+      break;
+    case "i":
+      e = "imes";
+      break;
+    case "a":
+      e = "ai";
+      break;
+    case "o":
+      o = "ober";
+      break;
+    case "u":
+      u = "ufat";
+      break;
+  }
+  mensajeEncriptado.push(e);
 }
